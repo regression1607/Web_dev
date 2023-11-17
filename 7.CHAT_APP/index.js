9b00db5d22b769e3de92a0afc976f14a93d410d9
EkanshRajput <92197239+EkanshRajput@users.noreply.github.com>
2023-11-17 21:09:34 +0530

    jebfe

--- 7.CHAT_APP/index.js
+++ 7.CHAT_APP/index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methoOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methoOverride("_method"));
 
 main()
   .then(() => {
@@ -51,10 +53,30 @@ app.post("/chats", async (req, res) => {
 });
 
 //edit route
-app.get("/chats/:id/edit",async(res,req)=>{
-    let {id}=req.params;
-    let chat=await(Chat.findById(id));
-    res.render("edit.ejs",{chat});
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
+});
+
+//update route
+app.put("/chats/:id", async (req, res) => {
+let { id } = req.params;
+let { msg: newMsg } = req.body;
+let updatedChat = await Chat.findByIdAndUpdate(id,
+{ msg: newMsg },
+ { runValidators: true, new: true }
+ );
+ console.log("/chats");
+ res.redirect("/chats");
+ });
+
+//delete 
+app.delete("/chats/:id",async(req,res)=>{
+  let {id}=req.params;
+  let deletedchat= await Chat.findByIdAndDelete(id);
+  console.log(deletedchat);
+  res.redirect("/chats");
 });
 
 app.get("/", (req, res) => {
